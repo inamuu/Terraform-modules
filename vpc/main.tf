a1961ab03becbfd1dd8b1f39bc69033ad0e69943
inamuu <8310973+inamuu@users.noreply.github.com>
2022-02-24 22:30:10 +0900

    add route table for private subnet

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,61 +1,67 @@
 resource "aws_vpc" "vpc" {
-    cidr_block           = var.cidr_block
-    enable_dns_hostnames = true
-    enable_dns_support   = true
-    instance_tenancy     = "default"
+  cidr_block           = var.cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  instance_tenancy     = "default"
 }
 
 resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
 }
 
 resource "aws_subnet" "public_1a" {
-    vpc_id            = aws_vpc.vpc.id
-    cidr_block        = var.cidr_block_public_1a
-    availability_zone = "ap-northeast-1a"
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.cidr_block_public_1a
+  availability_zone = "ap-northeast-1a"
 }
 
 resource "aws_subnet" "public_1c" {
-    vpc_id            = aws_vpc.vpc.id
-    cidr_block        = var.cidr_block_public_1c
-    availability_zone = "ap-northeast-1c"
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.cidr_block_public_1c
+  availability_zone = "ap-northeast-1c"
 }
 
 resource "aws_subnet" "private_1a" {
-    vpc_id            = aws_vpc.vpc.id
-    cidr_block        = var.cidr_block_private_1a
-    availability_zone = "ap-northeast-1a"
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.cidr_block_private_1a
+  availability_zone = "ap-northeast-1a"
 }
 
 resource "aws_subnet" "private_1c" {
-    vpc_id            = aws_vpc.vpc.id
-    cidr_block        = var.cidr_block_private_1c
-    availability_zone = "ap-northeast-1c"
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.cidr_block_private_1c
+  availability_zone = "ap-northeast-1c"
 }
 
 resource "aws_route_table" "igw" {
-    vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
 
-    route {
+  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
-    }
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
 }
 
 resource "aws_route_table_association" "public_1a" {
-    subnet_id      = aws_subnet.public_1a.id
-    route_table_id = aws_route_table.igw.id
+  subnet_id      = aws_subnet.public_1a.id
+  route_table_id = aws_route_table.igw.id
 }
 
 resource "aws_route_table_association" "public_1c" {
-    subnet_id      = aws_subnet.public_1c.id
-    route_table_id = aws_route_table.igw.id
+  subnet_id      = aws_subnet.public_1c.id
+  route_table_id = aws_route_table.igw.id
 }
 
 resource "aws_route_table_association" "private_1a" {
-    subnet_id      = aws_subnet.private_1a.id
+  subnet_id      = aws_subnet.private_1a.id
+  route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "private_1c" {
-    subnet_id      = aws_subnet.private_1c.id
+  subnet_id      = aws_subnet.private_1c.id
+  route_table_id = aws_route_table.private.id
 }
--- vpc/output.tf
+++ vpc/output.tf
@@ -1,19 +1,19 @@
 output "vpc_id" {
-    value = aws_vpc.vpc.id
+  value = aws_vpc.vpc.id
 }
 
 output "subnet_public_1a" {
-    value = aws_subnet.public_1a.id
+  value = aws_subnet.public_1a.id
 }
 
 output "subnet_public_1c" {
-    value = aws_subnet.public_1c.id
+  value = aws_subnet.public_1c.id
 }
 
 output "subnet_private_1a" {
-    value = aws_subnet.private_1a.id
+  value = aws_subnet.private_1a.id
 }
 
 output "subnet_private_1c" {
-    value = aws_subnet.private_1c.id
+  value = aws_subnet.private_1c.id
 }
