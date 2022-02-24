5b9d99cdad2701f52f9d21dedb9c3deb21a0cfdc
inamuu <8310973+inamuu@users.noreply.github.com>
2022-02-24 21:37:16 +0900

    create vpc modules

---
+++ vpc/main.tf
@@ -0,0 +1,61 @@
+resource "aws_vpc" "vpc" {
+    cidr_block           = var.cidr
+    enable_dns_hostnames = true
+    enable_dns_support   = true
+    instance_tenancy     = "default"
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_subnet" "public_1a" {
+    vpc_id            = aws_vpc.vpc.id
+    cidr_block        = var.cid_public_1a
+    availability_zone = "ap-northeast-1a"
+}
+
+resource "aws_subnet" "public_1c" {
+    vpc_id            = aws_vpc.vpc.id
+    cidr_block        = var.cid_public_1c
+    availability_zone = "ap-northeast-1c"
+}
+
+resource "aws_subnet" "private_1a" {
+    vpc_id            = aws_vpc.vpc.id
+    cidr_block        = var.cid_private_1a
+    availability_zone = "ap-northeast-1a"
+}
+
+resource "aws_subnet" "private_1c" {
+    vpc_id            = aws_vpc.vpc.id
+    cidr_block        = var.cidr_private_1c
+    availability_zone = "ap-northeast-1c"
+}
+
+resource "aws_route_table" "igw" {
+    vpc_id = aws_vpc.vpc.id
+
+    route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+    }
+}
+
+resource "aws_route_table_association" "public_1a" {
+    subnet_id      = aws_subnet.public_1a.id
+    route_table_id = aws_route_table.igw.id
+}
+
+resource "aws_route_table_association" "public_1c" {
+    subnet_id      = aws_subnet.public_1c.id
+    route_table_id = aws_route_table.igw.id
+}
+
+resource "aws_route_table_association" "private_1a" {
+    subnet_id      = aws_subnet.private_1a.id
+}
+
+resource "aws_route_table_association" "private_1c" {
+    subnet_id      = aws_subnet.private_1c.id
+}
---
+++ vpc/variables.tf
@@ -0,0 +1,5 @@
+variable "cidr" {}
+variable "cidr_public_1a" {}
+variable "cidr_public_1c" {}
+variable "cidr_private_1a" {}
+variable "cidr_private_1c" {}
